e2588bb167bc950592fb861381a5ef10f3135362
vasekbrozka <vasekbrozka@icloud.com>
2026-07-12 17:43:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const C = 'kcd2-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET' || !e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(C).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
